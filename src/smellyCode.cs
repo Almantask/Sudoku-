8b0c4cae234a0c42ea46ba345337bd5688e21f41
Kaisinel <almantusk@gmail.com>
2019-07-16 18:23:47 +0300

    Create smellyCode.cs
    Smelly code for CI/ codacy.

---
+++ src/smellyCode.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Smelly{
+
+  internal class SmellyClass{
+    public object ASJDKLADSJLKAD;
+    
+    public int[,] array1 = new int[,]
+    {
+      {1,2,3},
+      {1,2,3},
+      {1,2,3},
+      {1,2,3}
+    }
+    
+    public int[,] array2 = new int[,]
+    {
+      {1,2,3},
+      {1,2,3},
+      {1,2,3},
+      {1,2,3}
+    }
+    
+    public int[,] array3 = new int[,]
+    {
+      {1,2,3},
+      {1,2,3},
+      {1,2,3},
+      {1,2,3}
+    }
+    
+    public int[,] array4 = new int[,]
+    {
+      {1,2,3},
+      {1,2,3},
+      {1,2,3},
+      {1,2,3}
+    }
+  }
+}
