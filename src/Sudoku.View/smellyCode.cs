6340158ceee1b67badb77846e297a9fb62c34272
Almantas <almantusk@gmail.com>
2019-07-16 18:38:36 +0300

    Missing ;

--- src/Sudoku.View/smellyCode.cs
+++ src/Sudoku.View/smellyCode.cs
@@ -4,37 +4,38 @@ namespace Smelly{
 
   internal class SmellyClass{
     public object ASJDKLADSJLKAD;
-    
-    public int[,] array1 = new int[,]
-    {
+
+        public int[,] array1 = new int[,]
+        {
       {1,2,3},
       {1,2,3},
       {1,2,3},
       {1,2,3}
-    }
-    
+        };
+
+
     public int[,] array2 = new int[,]
     {
       {1,2,3},
       {1,2,3},
       {1,2,3},
       {1,2,3}
-    }
-    
-    public int[,] array3 = new int[,]
-    {
+    };
+
+        public int[,] array3 = new int[,]
+        {
       {1,2,3},
       {1,2,3},
       {1,2,3},
       {1,2,3}
-    }
-    
-    public int[,] array4 = new int[,]
-    {
+        };
+
+        public int[,] array4 = new int[,]
+        {
       {1,2,3},
       {1,2,3},
       {1,2,3},
       {1,2,3}
-    }
+        };
   }
 }
